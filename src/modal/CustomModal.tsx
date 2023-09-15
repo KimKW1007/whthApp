6d26206cc1c6926889183deb2899d542f2ca93b2
KimKW1007 <face676934@gmail.com>
2023-09-15 23:30:03 +0900

    feat : CustomModal

---
+++ src/modal/CustomModal.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Dimensions, Modal, Pressable, StyleSheet, Text, View } from 'react-native';
+import { theme } from '../theme/theme';
+import ModalBtn from './ModalBtn';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+
+
+const CustomModal = ({deleteToDo, deleteCancle} : {[key :string] : () =>void}) => {
+  return (
+    <Modal animationType="fade" transparent={true}>
+      <View style={styles.modalWrap}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>안내</Text>
+          <Text style={styles.modalDescription}>삭제하시겠습니까?</Text>
+          <View style={styles.modalBtnBox}>
+            <ModalBtn text='삭제' onPress={deleteToDo} />
+            <ModalBtn text='취소' onPress={deleteCancle} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default CustomModal;
+
+const styles = StyleSheet.create({
+  modalWrap: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalContainer: {
+    width: SCREEN_WIDTH - 100,
+    backgroundColor: '#fff',
+    borderRadius: 15
+  },
+  modalTitle: {
+    textAlign: 'center',
+    borderBottomColor: `${theme.grey}35`,
+    borderBottomWidth: 1,
+    paddingVertical: 10
+  },
+  modalDescription: {
+    paddingVertical: 30,
+    textAlign: 'center',
+    paddingHorizontal: 40
+  },
+  modalBtnBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderTopWidth: 1,
+    borderColor: `${theme.grey}35`
+  },
+  modalBtn: {
+    flexGrow: 1,
+    paddingVertical: 10
+  },
+  modalBtnText: {
+    textAlign: 'center'
+  }
+});
---
+++ src/modal/ModalBtn.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Pressable, StyleSheet, Text } from 'react-native'
+import { theme } from '../theme/theme'
+
+const ModalBtn = ({text , onPress} : {text : string, onPress : ()=> void}) => {
+  return (
+    <Pressable onPress={onPress} style={text === '삭제' ? { ...styles.modalBtn, borderRightWidth: 1, borderColor: `${theme.grey}35` }  :styles.modalBtn}>
+      <Text
+        style={{
+          ...styles.modalBtnText,
+          color: text === '삭제' ? '#FF6969' : '#279EFF'
+        }}>
+        {text}
+      </Text>
+    </Pressable>
+  )
+}
+
+export default ModalBtn
+
+const styles = StyleSheet.create({
+  modalBtn: {
+    flexGrow: 1,
+    paddingVertical: 10
+  },
+  modalBtnText: {
+    textAlign: 'center'
+  }
+});
