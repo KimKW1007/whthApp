aae23f0b2281844c32c700b5eff3ef3d0c94880a
KimKW1007 <face676934@gmail.com>
2023-09-15 23:29:52 +0900

    refactor : UDBtnBox - icon 및 style 변경 및 로직 변경

--- src/UDBtnBox.tsx
+++ src/UDBtnBox.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Fontisto, Octicons } from '@expo/vector-icons';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Fontisto, Octicons, Ionicons } from '@expo/vector-icons';
 import { theme } from './theme/theme';
 
-const UDBtnBox = ({ onPressDelete, onPressUpdate }: { [key: string]: () => void }) => {
+interface UBBtnBoxProps {
+  isEdit: boolean;
+  onPressDelete: () => void;
+  onPressUpdateSetting: () => void;
+  onPressUpdate: () => void;
+}
+
+const UDBtnBox = ({ isEdit, onPressDelete, onPressUpdateSetting, onPressUpdate }: UBBtnBoxProps) => {
   return (
-    <View style={{ ...styles.toDoTextBox, columnGap: 10 }}>
-      <TouchableOpacity style={{ width: 30 }} onPress={onPressUpdate}>
-        <Octicons name="pencil" size={24} color={theme.grey} />
-      </TouchableOpacity>
+    <View style={styles.toDoTextBox}>
+      <Pressable style={{ width: 30 }} onPress={isEdit ? onPressUpdate : onPressUpdateSetting}>
+        {isEdit ? (
+          <Ionicons name="ios-checkmark-circle-outline" size={24} color={theme.grey} />
+        ) : (
+          <Octicons name="pencil" size={24} color={theme.grey} />
+        )}
+      </Pressable>
       <TouchableOpacity onPress={onPressDelete}>
         <Fontisto name="trash" size={18} color={theme.grey} />
       </TouchableOpacity>
@@ -21,6 +32,8 @@ export default UDBtnBox;
 const styles = StyleSheet.create({
   toDoTextBox: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    columnGap: 10,
+    flexShrink: 0
   }
 });
