9711d075df1f0667f0e6e987aa38cae344368985
KimKW1007 <face676934@gmail.com>
2023-09-15 23:29:20 +0900

    refactor : ToDoTextBox - ellipsizeMode  및 detail 추가

--- src/ToDoTextBox.tsx
+++ src/ToDoTextBox.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 
-const ToDoTextBox = ({completed , text, onPress} : {completed :boolean, text :string, onPress : () => void}) => {
+interface ToDoTextBoxProps {
+  setDetailId: () => void;
+  completed: boolean;
+  text: string;
+  onPress: () => void;
+  isDetail: boolean;
+}
+
+const ToDoTextBox = ({ isDetail, setDetailId, completed, text, onPress }: ToDoTextBoxProps) => {
   return (
     <View style={styles.toDoTextBox}>
-      <TouchableOpacity style={styles.completedIconBox} onPress={onPress}>
+      <TouchableOpacity style={styles.checkIconBox} onPress={onPress}>
         {completed ? <Fontisto name="checkbox-active" size={18} color="white" /> : <Fontisto name="checkbox-passive" size={18} color="white" />}
       </TouchableOpacity>
-      <Text style={!completed ? styles.toDoText : { ...styles.toDoText, color: '#999', textDecorationLine: 'line-through' }}>{text}</Text>
+      {!completed && isDetail ? (
+        <Pressable onPress={setDetailId}>
+          <Text style={styles.toDoDetailText}>{text}</Text>
+        </Pressable>
+      ) : (
+        <Pressable>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            onPress={setDetailId}
+            style={!completed ? styles.toDoText : { ...styles.toDoText, color: '#999', textDecorationLine: 'line-through' }}>
+            {text}
+          </Text>
+        </Pressable>
+      )}
     </View>
   );
 };
@@ -17,15 +39,22 @@ export default ToDoTextBox;
 
 const styles = StyleSheet.create({
   toDoTextBox: {
+    width: '70%',
     flexDirection: 'row',
-    alignItems: 'center'
+  },
+  checkIconBox: {
+    width: 30,
+    marginTop : 1,
   },
   toDoText: {
     color: '#fff',
     fontSize: 16,
-    fontWeight: '600',
+    height: 21,
+    fontWeight: '600'
   },
-  completedIconBox :{
-    width: 30,
-  },
-})
+  toDoDetailText:{
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600'
+  }
+});
